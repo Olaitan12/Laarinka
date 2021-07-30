544af01fcfe378e641e85c3f5e4eaaaf97d17faf
Olaitan <olaitanaderoju@gmail.com>
2021-07-30 16:16:50 +0800

    Template Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { PlacesComponent } from './places/places.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
+    PlacesComponent
   ],
   imports: [
     BrowserModule,
